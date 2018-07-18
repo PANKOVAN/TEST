ded184833564124932ae15c95ff6c46c5ad04437
Панков Андрей <pankovan@yandex.ru>
2018-07-18 19:43:53 +0300

    2

--- MBuilder3X/Macros/appData.js
+++ MBuilder3X/Macros/appData.js
@@ -1,7 +1,7 @@
 ﻿({
     // $appData     - загрузка данных (используется для дальнейшей интерпретации json webix)
     // $data
-    //  url   - url
+    //  url   - url 11111111111
     $appData: function (p) {
         let json = m.main.getAppJson(p.name, {}, p.folder);
         return { data: json };
